bd782e198fd5d0d22600403e5c4b9726d6172dd1
RtzMark <marco.retizt@gmail.com>
2020-11-18 21:47:45 -0600

    Clase de respuesta general

---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Dominio/Respuesta.cs
@@ -0,0 +1,19 @@
+﻿namespace Axosnet.Recibos.Dominio
+{
+    /// <summary>
+    /// Se utiliza como respuesta en todas las peticiones realizadas a la api.
+    /// </summary>
+    /// <typeparam name="T">Informacion que se regresara al usuario (Listas, int, texto, etc)</typeparam>
+    public class Respuesta<T>
+    {
+        public bool error { get; set; }
+        public string mensaje { get; set; }
+        public T datos { get; set; }
+
+
+        public Respuesta()
+        {
+            error = false;
+        }
+    }
+}
