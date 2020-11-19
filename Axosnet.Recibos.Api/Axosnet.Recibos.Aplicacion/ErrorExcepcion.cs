d29ce8d95c9243f98d6894925cd48fdf717dbfe0
RtzMark <marco.retizt@gmail.com>
2020-11-19 02:10:40 -0600

    Procesar excepciones

---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Aplicacion/ErrorExcepcion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace Axosnet.Recibos.Aplicacion
+{
+    public class ErrorExcepcion : Exception
+    {
+        public HttpStatusCode Codigo { get; }
+        public string Error { get; }
+        public ErrorExcepcion(HttpStatusCode codigo, string error)
+        {
+            Codigo = codigo;
+            Error = error;
+        }
+    }
+}
