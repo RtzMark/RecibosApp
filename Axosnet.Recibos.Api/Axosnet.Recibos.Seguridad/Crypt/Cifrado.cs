3bbc81c6b0d2c1a2cd872338ddd248ab96766ee2
RtzMark <marco.retizt@gmail.com>
2020-11-18 22:15:34 -0600

    Clase de cifrado con SHA256

---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Seguridad/Crypt/Cifrado.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Axosnet.Recibos.Seguridad.Crypt
+{
+    public static class Cifrado
+    {
+        public static string EncryptSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            StringBuilder sb = new StringBuilder();
+            byte[] stream = null;
+
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++)
+            {
+                sb.AppendFormat("{0:x2}", stream[i]);
+            }
+
+
+            return sb.ToString();
+        }
+    }
+}
