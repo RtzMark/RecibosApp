1d7be0afd4a4e60bc62f6c34fac535387119abdb
RtzMark <marco.retizt@gmail.com>
2020-11-18 21:44:17 -0600

    Se agregan entidades de bd

---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Dominio/Entidad/Recibo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Axosnet.Recibos.Dominio.Entidad
+{
+    public class Recibo
+    {
+        public int Id { get; set; }
+        public string Proveedor { get; set; }
+        public decimal Monto { get; set; }
+        public string Moneda { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentario { get; set; }
+    }
+}
---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Dominio/Entidad/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Axosnet.Recibos.Dominio.Entidad
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public bool Activo { get; set; }
+    }
+}
