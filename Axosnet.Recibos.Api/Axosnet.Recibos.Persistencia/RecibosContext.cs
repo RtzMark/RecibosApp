a52ad4c913c105f8b2d1bdec214e6230e6ede4c1
RtzMark <marco.retizt@gmail.com>
2020-11-18 22:07:56 -0600

    Context Recibos

---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Persistencia/RecibosContext.cs
@@ -0,0 +1,27 @@
+﻿using Axosnet.Recibos.Dominio.Entidad;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Axosnet.Recibos.Persistencia
+{
+    public class RecibosContext : DbContext
+    {
+        public RecibosContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Recibo> Recibos { get; set; }
+    }
+}
