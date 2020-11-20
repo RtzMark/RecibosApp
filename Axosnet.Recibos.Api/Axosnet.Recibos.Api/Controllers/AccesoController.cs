1acc8083a350a0f84b1a41167a2ae12e29e53ed6
RtzMark <marco.retizt@gmail.com>
2020-11-20 02:00:57 -0600

    Controlador con acceso anonimo

--- Axosnet.Recibos.Api/Axosnet.Recibos.Api/Controllers/AccesoController.cs
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Api/Controllers/AccesoController.cs
@@ -1,11 +1,13 @@
 ﻿using Axosnet.Recibos.Aplicacion.Acceso;
 using Axosnet.Recibos.Dominio;
 using Axosnet.Recibos.Dominio.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Axosnet.Recibos.Api.Controllers
 {
     [Route("api/[controller]")]
+    [AllowAnonymous]
     [ApiController]
     public class AccesoController : ControllerBase
     {
