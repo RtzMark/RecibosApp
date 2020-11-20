5ce28d41a5e46c447709103d1d52b28f6911ef22
RtzMark <marco.retizt@gmail.com>
2020-11-20 01:37:03 -0600

    Controlador acceso y se agrega metdo login

---
+++ Axosnet.Recibos.Api/Axosnet.Recibos.Api/Controllers/AccesoController.cs
@@ -0,0 +1,26 @@
+﻿using Axosnet.Recibos.Aplicacion.Acceso;
+using Axosnet.Recibos.Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Axosnet.Recibos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccesoController : ControllerBase
+    {
+        private readonly IAccesoHelper _accesoHelper;
+
+
+        public AccesoController(IAccesoHelper accesoHelper)
+        {
+            _accesoHelper = accesoHelper;
+        }
+
+
+        [HttpPost("Login")]
+        public ActionResult<Respuesta<string>> Login([FromBody] string email, [FromBody] string clave)
+        {
+            return _accesoHelper.Login(email, clave);
+        }
+    }
+}
