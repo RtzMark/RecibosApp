a7697c07ec3225984af9873aa327405984fa6774
RtzMark <marco.retizt@gmail.com>
2020-11-21 15:30:38 -0600

    Implementacion de reducer y context

--- Axosnet.Recibos.Web/src/RecibosApp.js
+++ Axosnet.Recibos.Web/src/RecibosApp.js
@@ -1,64 +1,25 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./views/Login";
-import Axios from "axios";
-import {
-  setToken,
-  getToken,
-  eliminarToken,
-  initAxiosInterceptar,
-} from "./Helpers/auth-helpers";
+import React, { useReducer, useEffect } from "react";
 
-initAxiosInterceptar();
+import RutaRecibo from "./routes/RutaRecibo";
+
+import { AuthContext } from "./auth/auth-context";
+import AuthReducer from "./auth/auth-reducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("user")) || { logged: false };
+};
 
 const RecibosApp = () => {
-  const [usuario, setUsuario] = useState(null);
-  const [cargandoUsuario, setCargandoUsuario] = useState(true);
+  const [user, dispatch] = useReducer(AuthReducer, {}, init);
 
   useEffect(() => {
-    const cargarUsuario = async () => {
-      if (!getToken()) {
-        setCargandoUsuario(false);
-        return;
-      }
-
-      try {
-        const { error, mensaje, datos } = await Axios.get(
-          "http://localhost:52234/api/Usuario"
-        );
-        setUsuario(datos);
-      } catch (error) {}
-    };
-
-    cargarUsuario();
-  }, []);
-
-  const login = async (email, clave) => {
-    const { error, mensaje, datos } = await Axios.post(
-      "http://localhost:52234/api/Acceso/Login",
-      {
-        email,
-        clave,
-      }
-    );
-
-    if (!error) {
-      setUsuario(datos.usuario);
-      setToken(datos.token);
-    }
-  };
-
-  const logout = async () => {
-    setUsuario(null);
-    eliminarToken();
-  };
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
 
   return (
-    <div>
-      <Navbar />
-      <Login login={login} />
-      <div>{JSON.stringify(usuario)}</div>
-    </div>
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <RutaRecibo />
+    </AuthContext.Provider>
   );
 };
 
