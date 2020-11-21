41fbb8ff183fd27a17723214bdabd3d00125a18c
RtzMark <marco.retizt@gmail.com>
2020-11-21 15:29:40 -0600

    Vistas recibo y usuarios (simples)

---
+++ Axosnet.Recibos.Web/src/views/Recibos/Recibos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../../components/Main/Main";
+
+const Recibos = () => {
+  return (
+    <Main center>
+      <h1>Recibos</h1>
+    </Main>
+  );
+};
+
+export default Recibos;
---
+++ Axosnet.Recibos.Web/src/views/Usuarios/Usuarios.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../../components/Main/Main";
+
+const Usuarios = () => {
+  return (
+    <Main center>
+      <h1>Usuarios</h1>
+    </Main>
+  );
+};
+
+export default Usuarios;
