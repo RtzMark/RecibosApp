6750c9c8fad4648a390066f81478fa3a1c392ad9
RtzMark <marco.retizt@gmail.com>
2020-11-21 00:13:26 -0600

    Carga inicial proyecto web (Navbar y Main)

---
+++ Axosnet.Recibos.Web/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./styles/Forms.css";
+import RecibosApp from "./RecibosApp";
+
+ReactDOM.render(<RecibosApp />, document.getElementById("root"));
