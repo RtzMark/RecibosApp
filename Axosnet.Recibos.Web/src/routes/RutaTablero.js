09bcda1059e40646dee4f40276e1d83f21d1cdfb
RtzMark <marco.retizt@gmail.com>
2020-11-22 22:10:32 -0600

    Implementacion de mensaje error

--- Axosnet.Recibos.Web/src/routes/RutaTablero.js
+++ Axosnet.Recibos.Web/src/routes/RutaTablero.js
@@ -1,17 +1,38 @@
-import React from "react";
-import Navbar from "../components/Navbar/Navbar";
+import React, { useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+
+import Navbar from "../components/Navbar/Navbar";
+import Error from "../components/Errorbar/Error";
+
 import Recibos from "../views/Recibos/Recibos";
 import Usuarios from "../views/Usuarios/Usuarios";
 
 const RutaTablero = () => {
+  const [error, setError] = useState();
+
+  const mostrarError = (mensaje) => {
+    setError(mensaje);
+  };
+
+  const ocultarError = () => {
+    setError(null);
+  };
+
   return (
     <>
       <Navbar />
+      <Error mensaje={error} esconderError={ocultarError} />
       <div>
         <Switch>
-          <Route exact path="/recibos" component={Recibos} />
-          <Route exact path="/usuarios" component={Usuarios} />
+          <Route
+            path="/recibos"
+            render={(props) => <Recibos mostrarError={mostrarError} />}
+            default
+          />
+          <Route
+            path="/usuarios"
+            render={(props) => <Usuarios mostrarError={mostrarError} />}
+          />
           <Redirect to="/recibos" />
         </Switch>
       </div>
