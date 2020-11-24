c3fb9bf1be3e0d6eca165787f5caec4b024d1474
RtzMark <marco.retizt@gmail.com>
2020-11-23 19:55:50 -0600

    Nueva ruta para inicio y cambo en import de error

--- Axosnet.Recibos.Web/src/routes/RutaTablero.js
+++ Axosnet.Recibos.Web/src/routes/RutaTablero.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Navbar from "../components/Navbar/Navbar";
-import Error from "../components/Errorbar/Error";
+import { Error } from "../components/Errorbar/Error";
 
-import Recibos from "../views/Recibos/Recibos";
-import Usuarios from "../views/Usuarios/Usuarios";
+import Recibos from "../views/Recibos";
+import Usuarios from "../views/Usuarios";
+import Home from "../views/Home";
 
 const RutaTablero = () => {
   const [error, setError] = useState();
@@ -24,16 +25,16 @@ const RutaTablero = () => {
       <Error mensaje={error} esconderError={ocultarError} />
       <div>
         <Switch>
+          <Route path="/Inicio" render={(props) => <Home />} default />
           <Route
             path="/recibos"
             render={(props) => <Recibos mostrarError={mostrarError} />}
-            default
           />
           <Route
             path="/usuarios"
             render={(props) => <Usuarios mostrarError={mostrarError} />}
           />
-          <Redirect to="/recibos" />
+          <Redirect to="/Inicio" />
         </Switch>
       </div>
     </>
