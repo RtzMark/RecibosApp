bbb40a42d5c93e9dd849e2e517a2a20eb1bf101a
RtzMark <marco.retizt@gmail.com>
2020-11-23 19:53:58 -0600

    Ajuste en dispatch, se elimina proceso de consulta de usuario

--- Axosnet.Recibos.Web/src/RecibosApp.js
+++ Axosnet.Recibos.Web/src/RecibosApp.js
@@ -1,54 +1,26 @@
 import React, { useReducer, useEffect } from "react";
 
-import HttpCliente, { initAxiosInterceptar } from "./services/HttpCliente";
+import { initAxiosInterceptar } from "./services/HttpCliente";
 import { getToken } from "./auth/auth-helpers";
 import { AuthContext } from "./auth/auth-context";
 import AuthReducer from "./auth/auth-reducer";
-import { types } from "./types/types";
 
 import RutaRecibo from "./routes/RutaRecibo";
 
 initAxiosInterceptar();
 
 const cargarUsuario = () => {
-  if (!getToken()) {
-    return { logged: false };
-  }
-
-  return { logged: true };
+  return getToken() && localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : { logged: false };
 };
 
 const RecibosApp = () => {
   const [user, dispatch] = useReducer(AuthReducer, {}, cargarUsuario);
 
   useEffect(() => {
-    const cargarUsuario = async () => {
-      if (user.logged) {
-        try {
-          const { error, datos } = await HttpCliente.get("Usuario");
-
-          if (error) {
-            dispatch({
-              type: types.logout,
-            });
-          } else {
-            dispatch({
-              type: types.login,
-              payload: {
-                email: datos.email,
-              },
-            });
-          }
-        } catch (error) {
-          dispatch({
-            type: types.logout,
-          });
-        }
-      }
-    };
-
-    cargarUsuario();
-  }, [user.logged]);
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
 
   return (
     <AuthContext.Provider value={{ user, dispatch }}>
